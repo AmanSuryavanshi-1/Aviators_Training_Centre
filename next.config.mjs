f4cfaf17949264cb6d2865b42223e42aef700db8
amansuryavanshi2812 <amansuryavanshi2812@gmail.com>
2025-04-30 20:30:53 +0000

    next conf update

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-      // Add the following lines if they're not already present.
-      // it may be already present in file.
-      // add it if its not.
-      ppr: true, // this is for partial pre rendering
-    },
-      // the below two are optional they will collect more data and give more insights
-    speedInsights: true,
-    webVitals: true,
-  };
+const nextConfig = {};
 
 export default nextConfig;
